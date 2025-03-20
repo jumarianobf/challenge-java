746ace337eb5ff34bc0268323ded82ed95558518
Julia <jumarianobf@gmail.com>
2025-03-19 21:29:19 -0300

    Projeto Iniciado

---
+++ src/main/java/br/com/fiap/challenge/ChallengeOdontoprevApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeOdontoprevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeOdontoprevApplication.class, args);
+	}
+
+}
